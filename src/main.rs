140440f367f83c872d892240cdb6bd389d02c607
Maurdekye <ncolaprete@gmail.com>
2023-10-04 17:56:47 -0400

    implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,145 @@
+use std::collections::HashMap;
+use clap::Parser;
+use reqwest::blocking::get;
+use scraper::{Html, Selector};
+use regex::Regex;
+
+#[derive(Parser, Debug)]
+struct Args {
+    // Ratingupdate matchups url
+    #[arg(short, long, default_value = "http://ratingupdate.info/matchups")]
+    url: String,
+
+    // maximum iterations of the algorithm to run
+    #[arg(short, long, default_value_t = 5000)]
+    iters: usize,
+
+    // Wait until the final scores settle this much before giving a final answer
+    #[arg(short, long, default_value_t = 0.00000f64)]
+    max_settle: f64
+}
+
+#[derive(Debug)]
+struct MatchupData {
+    matchups: HashMap<String, HashMap<String, f64>>
+}
+
+fn get_matchups_from_ratingupdate(url: String) -> Vec<(Option<String>, MatchupData)> {
+    let table_selector = Selector::parse("div.table-container").unwrap();
+    let inner_table_selector = Selector::parse("table tbody").unwrap();
+    let row_selector = Selector::parse("tr").unwrap();
+    let cell_selector = Selector::parse("th,td span").unwrap();
+    let cell_regex = Regex::new(r"(\d+(\.\d?))").unwrap();
+
+    println!("Fetching matchup content");
+
+    let response = get(url);
+    let body = response.expect("Failed to fetch site content").text().expect("Failed to decode site content");
+    let dom = Html::parse_document(&body);
+
+    println!("Parsing");
+
+    let mut matchup_sets = Vec::new();
+
+    for outer_table in dom.select(&table_selector) {
+        let mut characters: Vec<String> = Vec::new();
+        let mut data: Vec<Vec<f64>> = Vec::new();
+
+        // parse into intermediate format
+        let table = outer_table.select(&inner_table_selector).next().unwrap();
+        let mut row_iter = table.select(&row_selector).into_iter();
+        row_iter.next();
+        for row in row_iter {
+            let mut cell_iter = row.select(&cell_selector).into_iter();
+            let header_cell = cell_iter.next().unwrap();
+            characters.push(header_cell.inner_html());
+            let mut new_data_row: Vec<f64> = Vec::new();
+            for cell in cell_iter {
+                let content = cell.inner_html();
+                let capture = cell_regex.captures(&content).unwrap().get(0).unwrap();
+                let value: f64 = capture.as_str().parse().unwrap();
+                new_data_row.push(value / 100f64);
+            }
+            data.push(new_data_row);
+        }
+
+        // parse into final format
+        let mut matchups: HashMap<String, HashMap<String, f64>> = HashMap::new();
+
+        for (i, char) in characters.iter().enumerate() {
+            let mut matchup: HashMap<String, f64> = HashMap::new();
+            for (j, match_value) in data[i].iter().enumerate() {
+                if i != j {
+                    matchup.insert(characters[j].clone(), data[j][i]);
+                }
+            }
+            matchups.insert(char.clone(), matchup);
+        }
+
+        // find name
+        let mut name = None;
+        // for sibling in outer_table.prev_siblings() {
+        //     match sibling.value().as_element() {
+        //         None => {},
+        //         Some(elem) => if elem.name() == "h3" {
+        //             println!("{:?}", ElementRef::try_from(elem));
+        //             name = Some(elem.name().to_string());
+        //             break;
+        //         }
+        //     }
+        // }
+
+        matchup_sets.push((name, MatchupData { matchups: matchups }));
+    }
+
+    matchup_sets
+}
+
+fn compute_tiers(data: MatchupData, max_iters: usize, max_settle: f64) -> (usize, f64, HashMap<String, f64>) {
+    let mut scores: HashMap<String, f64> = HashMap::new();
+    for char in data.matchups.keys() {
+        scores.insert(char.clone(), 0f64);
+    }
+    let mut grand_mult = 1f64;
+    let mut iters = 0usize;
+
+    loop {
+        let mut new_scores: HashMap<String, f64> = HashMap::new();
+        for char in scores.keys() {
+            let sub_scores = data.matchups.get(char).unwrap().iter().map(|(opponent, score)| (2f64*score - 1f64) * 2f64.powf(scores[opponent]) * grand_mult);
+            new_scores.insert(char.clone(), sub_scores.sum());
+        }
+        scores = new_scores;
+        let last_grand_mult = grand_mult;
+        grand_mult = 1f64 / scores.values().map(|v| v.abs()).sum::<f64>();
+        iters += 1;
+        if (last_grand_mult - grand_mult).abs() < max_settle {
+            break;
+        } else if iters >= max_iters {
+            break;
+        }
+    }
+
+    (iters, grand_mult, scores)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let matchup_sets = get_matchups_from_ratingupdate(args.url);
+
+    for (name, matchups) in matchup_sets {
+        match name {
+            Some(chart_name) => println!("{} tier list", chart_name),
+            None => {}
+        }
+        println!("Computing tiers...");
+        let (iters, grand_mult, tiers) = compute_tiers(matchups, args.iters, args.max_settle);
+        let mut sorted = tiers.into_iter().collect::<Vec<(String, f64)>>();
+        sorted.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+        println!("after {} iters, with a grand multiplier of {}:", iters, grand_mult);
+        for (char, score) in sorted {
+            println!("{}: {}", char, score);
+        }
+    }
 }
